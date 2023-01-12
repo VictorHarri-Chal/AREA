08e8196f0906ff2004c73e30ffe33ce10ca5e77b
TitouanDsch <titouan.deschanels@epitech.eu>
2023-01-12 18:25:54 +0100

    [UPDATE] Add more icons

--- area/src/components/IconSidebarL/IslData.js
+++ area/src/components/IconSidebarL/IslData.js
@@ -4,18 +4,33 @@ import { Icon } from '@iconify/react';
 export const IslData = [
     {
         title: 'Discord',
-        icon : <Icon icon="logos:discord-icon" />,
+        icon : <Icon icon="mdi:discord" />,
         link : 'discord'
     },
     {
         title: 'Github',
-        icon : <Icon icon="jam:github" />,
+        icon : <Icon icon="mdi:github" />,
         link : 'github'
     },
     {
         title: 'Twitter',
         icon : <Icon icon="mdi:twitter" />,
         link : 'twitter'
+    },
+    {
+        title: 'Spotify',
+        icon : <Icon icon="mdi:spotify" />,
+        link : 'spotify'
+    },
+    {
+        title: 'Youtube',
+        icon : <Icon icon="mdi:youtube" />,
+        link : 'youtube'
+    },
+    {
+        title: 'Twitch',
+        icon : <Icon icon="mdi:twitch" />,
+        link : 'twitch'
     }
 ]
 
