ac8f87648294ec4c23d4d05042f58010175711d6
Blynqs <baptite.friboulet@epitech.eu>
2023-01-12 12:15:39 +0100

    [ADD] Added api fetch test

--- area/src/App.js
+++ area/src/App.js
@@ -3,10 +3,17 @@ import './App.css';
 import Home from './pages'
 import { BrowserRouter as Router } from 'react-router-dom';
 
+function fetchAPI() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
+
 function App() {
     return (
         <Router>
             <Home />
+            <button onClick={(event)=>fetchAPI()}>La fève</button>
         </Router>
     );
 }
