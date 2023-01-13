a6761eec916bdcfa7cd096efb3f679b967069512
TitouanDsch <titouan.deschanels@epitech.eu>
2023-01-13 18:08:21 +0100

    [UPDATE] Add ConnectBtn

--- area/src/components/BlocSidebar/BSElements.js
+++ area/src/components/BlocSidebar/BSElements.js
@@ -45,4 +45,58 @@ export const BSAppName = styled.div`
     font-size : 30px;
     font-weight: bold;
     color: ${props => props.color};
+    transition: all 0.2s ease-in-out;
 `;
+
+export const BSConnectBtn = styled.div`
+    border-radius: 50px;
+    background: ${props => props.color};
+    white-space: nowrap;
+    padding: 10px 22px;
+    color: #010606;
+    font-size: 25px;
+    font-weight: bold;
+    outline: none;
+    border: none;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+    text-decoration: none;
+    position: absolute;
+    transform: translate(-50%, -50%);
+    top : 130px;
+    left : 150px;
+
+    &:hover {
+        transition: all 0.8s ease-in-out;
+        background: #fff;
+        color: #010606;
+    }
+
+    opacity: ${props => props.login === undefined ? 0 : (props.login === true ? 0 : 100)}%;
+`;
+
+export const BSConnected = styled.div`
+    white-space: nowrap;
+    padding: 10px 22px;
+    color: #010606;
+    font-size: 25px;
+    font-weight: bold;
+    outline: none;
+    border: none;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+    text-decoration: none;
+    position: absolute;
+    transform: translate(-50%, -50%);
+    top : 130px;
+    left : 150px;
+
+    &:hover {
+        transition: all 0.8s ease-in-out;
+        background: #fff;
+        color: #010606;
+    }
+
+    opacity: ${props => props.login === undefined ? 0 : (props.login === false ? 0 : 100)}%;
+`;
+
--- area/src/components/BlocSidebar/index.js
+++ area/src/components/BlocSidebar/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BSAppName, BSContainer, BSLogoApp } from './BSElements'
+import { BSAppName, BSContainer, BSLogoApp, BSConnectBtn, BSConnected } from './BSElements'
 
 const BlocSidebar = ({ appSelected }) => {
     if (appSelected.key === undefined) return (<></>)
@@ -7,6 +7,8 @@ const BlocSidebar = ({ appSelected }) => {
         <BSContainer>
             <BSLogoApp color={appSelected.color}>{appSelected.icon}</BSLogoApp>
             <BSAppName color={appSelected.color}>{appSelected.title}</BSAppName>
+            <BSConnectBtn color={appSelected.color} login={appSelected.login}>Connect</BSConnectBtn>
+            <BSConnected color={appSelected.color} login={appSelected.login}>Connected</BSConnected>
         </BSContainer>
     )
 }
