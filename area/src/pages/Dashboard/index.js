b55e34029dc62311c94d044f8807b5352902ac40
Victor Harri-Chal <victor.harri-chal@epitech.eu>
2023-01-13 18:53:49 +0100

    [ADD] Canva to move rectangles fast

--- area/src/pages/Dashboard/index.js
+++ area/src/pages/Dashboard/index.js
@@ -1,14 +1,15 @@
 import React, {useState} from 'react'
 import Navbar from '../../components/Navbar'
-import DragableRectangle from '../../components/DragableRectangle'
+import DragableRectangle from '../../components/Playground'
 import IconSidebarL from '../../components/IconSidebarL'
+import Playground from '../../components/Playground'
 
 const Dashboard = () => {
     return (
         <>
-            <DragableRectangle />
             <Navbar isOpen={false} isInDashboard={true}/>
             <IconSidebarL />
+            <Playground />
         </>
     )
 }
