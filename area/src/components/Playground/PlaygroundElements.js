57d8e06fa9760807a7b3da814f9c7ec1d700397f
Victor Harri-Chal <victor.harri-chal@epitech.eu>
2023-01-16 11:59:18 +0100

    [ADD] New version of playground & trailing spaces fix

--- area/src/components/Playground/PlaygroundElements.js
+++ area/src/components/Playground/PlaygroundElements.js
@@ -1,15 +1,25 @@
 import styled from 'styled-components';
 
-export const DragableRectangleContainer = styled.div`
-  width: 100%;
-  height: 100%;
+export const StyledButton = styled.button`
+  left: 50%;
+  top: 50%;
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  border-radius: 5px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  transition: all 0.3s ease;
   position: absolute;
-`;
 
-export const RectangleContainer = styled.div`
-  width: 10%;
-  height: 8%;
-  background: red;
-  position: absolute;
-  border-radius: 20px
+  &:hover {
+    background-color: #3e8e41; /* Darker green */
+    transform: scale(1.05);
+  }
 `;
