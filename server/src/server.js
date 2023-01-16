bcdae3478d1d26b5b22b60c3b8cab221376d580e
Blynqs <baptite.friboulet@epitech.eu>
2023-01-16 17:18:51 +0100

    [ADD] Added server init

---
+++ server/src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
+const port = 4000;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
