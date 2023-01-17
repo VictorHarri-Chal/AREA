a0cf2bc024e7a469591aefb6d5b7556f24590933
Kéon Savic <keon.savic@epitech.eu>
2023-01-17 14:39:56 +0100

    [ADD] - basic running server on localhost port 6969

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const PORT = 6969;
+
+
+var corsOpt = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOpt));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
-  });
+});
 
-const port = 4000;
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
 });
