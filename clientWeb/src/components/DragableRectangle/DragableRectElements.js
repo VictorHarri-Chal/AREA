c4c3c645a9d19a7d8f89af8083d1668cc0c42b6c
TitouanDsch <titouan.deschanels@epitech.eu>
2023-01-17 17:24:02 +0100

    [UPDATE] delete useless styles

--- clientWeb/src/components/DragableRectangle/DragableRectElements.js
+++ clientWeb/src/components/DragableRectangle/DragableRectElements.js
@@ -5,27 +5,8 @@ export const DragableRectangleStyle = styled.div`
     height: ${props => props.height}px;
     left: ${props => props.left}px;
     top: ${props => props.top}px;
-    background: blue;
+    background: ${props => props.color};
     position: absolute;
     border-radius: 15px;
-`;
-
-export const DragableRectangleStyle2 = styled.div`
-    width: ${props => props.width}px;
-    height: ${props => props.height}px;
-    left: ${props => props.left}px;
-    top: ${props => props.top}px;   
-    background: red;
-    position: absolute;
-    border-radius: 15px;
-`;
-
-export const DragableRectangleStyle3 = styled.div`
-    width: ${props => props.width}px;
-    height: ${props => props.height}px;
-    left: ${props => props.left}px;
-    top: ${props => props.top}px;
-    background: green;
-    position: absolute;
-    border-radius: 15px;
+    z-index : -3;
 `;
