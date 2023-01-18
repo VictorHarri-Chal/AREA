c6a7309165e1235291324398647fbab28fe1f78f
Kéon Savic <keon.savic@epitech.eu>
2023-01-18 18:44:41 +0100

    [ADD] - sign in function and verifications

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -60,3 +60,48 @@ exports.signup = (req, res) => {
         }
     });
 };
+
+exports.signin = (req, res) => {
+    User.findOne({
+      username: req.body.username
+    }).populate("roles", "-__v")
+      .exec((err, user) => {
+        if (err) {
+            res.status(500).send({ message: err });
+            return;
+        }
+
+        if (!user) {
+            return res.status(404).send({ message: "This User does not exist" });
+        }
+
+        var passwordIsValid = bcrypt.compareSync(
+            req.body.password,
+            user.password
+        );
+
+        if (!passwordIsValid) {
+            return res.status(401).send({
+                accessToken: null,
+                message: "Wrong Password"
+            });
+        }
+
+        var authorities = [];
+        var token = jwt.sign({ id: user.id }, config.secret, {
+            expiresIn: 86400 // L'équivalent d'un jour en secondes
+        });
+
+        for (let i = 0; i < user.roles.length; i++) {
+            authorities.push("role_permissions_" + user.roles[i].name);
+        }
+
+        res.status(200).send({
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            roles: authorities,
+            accessToken: token
+        });
+    });
+};
