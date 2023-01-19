3f6879c554df32aa0ca59cde8e582df2f816d23f
TitouanDsch <titouan.deschanels@epitech.eu>
2023-01-19 15:30:55 +0100

    [FIX] MobileIcon

--- web/src/components/AppSidebar/ASElements.js
+++ web/src/components/AppSidebar/ASElements.js
@@ -83,7 +83,7 @@ export const MobileIcon = styled.div`
     @media screen and (max-width: 1000px) {
         display: block;
         position: absolute;
-        top: 30px;
+        top: 100px;
         ${props => props.close === false ? `left: 30px;` : `right : 30px;`}
         font-size: 45px;
         cursor: pointer;
