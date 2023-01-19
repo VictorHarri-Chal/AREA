8a188b56dd9624aa8aa4445f2e212de209e31acb
TitouanDsch <titouan.deschanels@epitech.eu>
2023-01-19 18:28:21 +0100

    [ADD] Blocs data

---
+++ web/src/components/Playground/BlocsData.js
@@ -0,0 +1,20 @@
+export const BlocsData = [
+    {
+        id : '1',
+        x : 0,
+        y : 0,
+        key : 'spotify_newStream',
+    },
+    {
+        id : '2',
+        x : 150,
+        y: 150,
+        key : 'discord_sendPrivateMessage',
+    },
+    {
+        id : '3',
+        x : 300,
+        y : 300,
+        key : 'github_createIssue',
+    }
+]
