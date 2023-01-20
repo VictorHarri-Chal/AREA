fd9f0dec20205ac063896ae2d17c28a66a695c21
Blynqs <baptite.friboulet@epitech.eu>
2023-01-20 15:20:37 +0100

    [UPD] Parsing api github

--- server/server.js
+++ server/server.js
@@ -1,18 +1,51 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
 const User = require('./src/models/user.model.js');
-const newUser = new User({
-  username: "example_username",
-  email: "example@email.com"
-});
-
 const utils = require('./src/utils/utils.js');
 
+const axios = require('axios');
+
+async function checkGithubTrigger(schema) {
+    const repositoryName = schema.repositoryName;
+    const action = schema.action;
+    const access_token = schema.access_token;
+    let url = '';
+    if (action === "push") {
+    url = `https://api.github.com/repos/${repositoryName}/events`;
+    } else if (action === "issue") {
+    url = `https://api.github.com/repos/${repositoryName}/issues`;
+    } else {
+    console.log("Invalid action provided. Please provide a valid action.");
+    }
+
+    try {
+        const response = await axios.get(url, {
+            headers: {
+            'Authorization': 'Token ' + access_token
+            }
+        });
+    if (action === "push") {
+        // console.log('data:', response.data);
+        // const revdata = response.data.reverse();
+        const pushEvent = response.data.find(event => event.type === 'PushEvent');
+        if(pushEvent) {
+            // console.log('commits: ', pushEvent.payload.commits);
+            const commits = pushEvent.payload.commits.reverse();
+            console.log("Push detected on repository: " + repositoryName + " with commit: " + commits[0].message);
+        }
+    } else if (action === "issue") {
+        console.log("New issue detected on repository: " + repositoryName);
+    }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
 function serverProcess() {
-    // utils.deleteUsers();
-    // utils.addUser(newUser.username, newUser.email);
-    // utils.displayUsers();
+    setInterval(() => {
+        checkGithubTrigger({ repositoryName: "VictorHarri-Chal/AREA", action: "push", access_token: "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" });
+    }, 3000);
 }
 
 function initDatabase() {
