a755deb9235d7460bb4ad8df6e565087d566ae2d
Victor Harri-Chal <victor.harri-chal@epitech.eu>
2023-02-14 16:42:50 +0100

    [ADD] Colors, [FIX] connected issue, [ADD] area interface

--- mobile/src/pages/Dashboard/index.js
+++ mobile/src/pages/Dashboard/index.js
@@ -5,33 +5,67 @@ import Icon from 'react-native-vector-icons/FontAwesome5';
 const Dashboard = () => {
   const [menuVisible, setMenuVisible] = useState(false);
   const [selectedButton, setSelectedButton] = useState(null);
+  const [currentBlocType, setCurrentBlocType] = useState("action_blocs");
 
-  const menuButtons = [
+  const [menuButtons, setMenuButtons] = useState([
     {
       name: "Discord",
-      icon: "discord"
-    },  
+      icon: "discord",
+      color: "#7289da",
+      login: false,
+      action_blocs : "Get a private message",
+      reaction_blocs : "Send a private message"
+    },
     {
       name: "Github",
-      icon: "github"
+      icon: "github",
+      color: "#99aab5",
+      login: false,
+      action_blocs : "New commit",
+      reaction_blocs : "Create an issue"
     },
     {
       name: "Twitter",
-      icon: "twitter"
+      icon: "twitter",
+      color: "#00acee",
+      login: false,
+      action_blocs : "New tweet",
+      reaction_blocs : "Send a tweet"
     },
     {
       name: "Spotify",
-      icon: "spotify"
+      icon: "spotify",
+      color: "#1ed760",
+      login: false,
+      action_blocs : "New stream",
+      reaction_blocs : "Create a playlist"
     },
     {
       name: "Youtube",
-      icon: "youtube"
+      icon: "youtube",
+      color: "#fe0000",
+      login: false,
+      action_blocs : "New youtube video",
+      reaction_blocs : "Post a commentary"
     },
     {
       name: "Twitch",
-      icon: "twitch"
+      icon: "twitch",
+      color: "#6441a5",
+      login: false,
+      action_blocs : "New stream on Twitch",
+      reaction_blocs : "Post a commentary"
     },
-  ];
+  ]);
+
+  const defineBlocType = () => {
+    if (currentBlocType === "action_blocs") {
+      return menuButtons.find(button => button.name === selectedButton).action_blocs;
+    } else if (currentBlocType === "reaction_blocs") {
+      return menuButtons.find(button => button.name === selectedButton).reaction_blocs;
+    }
+    return null;
+  }
 
   return (
     <View style={styles.container}>
@@ -50,25 +84,56 @@ const Dashboard = () => {
                 ]}
                 onPress={() => setSelectedButton(button.name)}
               >
-                <Icon name={button.icon} size={30} color="#000" />
-                <View style={[styles.buttonIndicator, selectedButton === 'Discord' && {backgroundColor: 'green'}]} />
+                <Icon name={button.icon} size={30} color={button.color} />
+                {console.log(menuButtons)}
+                <View style={[styles.buttonIndicator, { backgroundColor: button.login ? 'green' : 'red' }]} />
               </TouchableOpacity>
             ))}
           </View>
           <View style={styles.menuRight}>
             {selectedButton ? (
               <>
-                <Icon style={styles.iconeMenuRight} name={menuButtons.find(button => button.name === selectedButton).icon} size={30} color="#fff" />
+                <Icon style={styles.iconeMenuRight} name={menuButtons.find(button => button.name === selectedButton).icon} size={30} color={menuButtons.find(button => button.name === selectedButton).color}/>
                 <Text style={styles.menuText}>{selectedButton}</Text>
-                <TouchableOpacity
+                {menuButtons.find(button => button.name === selectedButton).login === false ? (
+                  <TouchableOpacity
                   style={styles.menuButton}
                   onPress={() => {
-                    setMenuVisible(false);
                     setSelectedButton(null);
+                    menuButtons.find(button => button.name === selectedButton).login = true;
+                    setMenuButtons(menuButtons);
                   }}
-                >
-                  <Text style={styles.menuButtonText}>Connect</Text>
-                </TouchableOpacity>
+                  >
+                    <Text style={styles.menuButtonText}>Connect</Text>
+                  </TouchableOpacity>
+                  ) : (
+                    <>
+                      <TouchableOpacity class="actionButton"
+                      style={styles.actionButton}
+                      onPress={() => {
+                        setCurrentBlocType("action_blocs");
+                      }}
+                      >
+                        <Text style={styles.actionReactionButtonText} >Action</Text>
+                      </TouchableOpacity>
+                      <TouchableOpacity class="reactionButton"
+                      style={styles.reactionButton}
+                      onPress={() => {
+                        setCurrentBlocType("reaction_blocs");
+                      }}
+                      >
+                        <Text style={styles.actionReactionButtonText} >Reaction</Text>
+                      </TouchableOpacity>
+                      <TouchableOpacity class="rectangleButton"
+                      style={[styles.rectangleButton, { backgroundColor: menuButtons.find(button => button.name === selectedButton).color}]}
+                      onPress={() => {
+                        setMenuVisible(false);
+                      }}
+                      >
+                        <Text style={styles.actionReactionButtonText}>{defineBlocType()}</Text>
+                      </TouchableOpacity>
+                    </>
+                  )}
               </>
             ) : (
               <Text style={styles.noButtonSelectedText}>No button selected</Text>
@@ -143,8 +208,34 @@ const styles = StyleSheet.create({
         width: 10,
         height: 10,
         borderRadius: 5,
-        backgroundColor: 'red',
       },
+    actionButton: {
+      position: 'absolute',
+      top: 90,
+      left: 40,
+      padding: 7,
+      backgroundColor: 'lightgray',
+      borderRadius: 6,
+    },
+    reactionButton: {
+      position: 'absolute',
+      top: 90,
+      left: 150,
+      padding: 7,
+      backgroundColor: 'lightgray',
+      borderRadius: 6,
+    },
+    actionReactionButtonText: {
+      color: 'black',
+      fontSize: 20,
+    },
+    rectangleButton: {
+      position: 'absolute',
+      alignItems: 'center',
+      top: 150,
+      padding: 7,
+      borderRadius: 4,
+    },
   });
-  
+
   export default Dashboard;
