845bcb5a9edf63e7c05131ac849f3b46eaa4f660
TitouanDsch <titouan.deschanels@epitech.eu>
2023-02-14 17:11:11 +0100

    [UPDATE] good profile display

--- web/src/components/ProfileDDM/index.js
+++ web/src/components/ProfileDDM/index.js
@@ -1,13 +1,32 @@
 import React from "react";
 import { ProfileDDMContainer, ProfilePicture, ProfileName, ProfileEmail, Separator, Button } from "./ProfileDDMElements";
 
-const ProfileDDM = ( {profileOpen, toggleProfile, x, y, userData} ) => {
+const ProfileDDM = ( {profileOpen, toggleProfile, x, y} ) => {
+
+    const [username, setUsername] = React.useState('');
+    const [email, setEmail] = React.useState('');
+    const [initials, setInitials] = React.useState('');
+
+    React.useEffect(() => {
+        if (username === '') {
+            setUsername(sessionStorage.username)
+        }
+
+        if (email === '') {
+            setEmail(sessionStorage.email)
+        }
+
+        if (initials === '') {
+            setInitials(sessionStorage.initials)
+        }
+    }, [username, email, initials]);
+
     if (!profileOpen) return null;
     return (
         <ProfileDDMContainer x={x} y={y}>
-            <ProfilePicture x={x} y={y} on onClick={toggleProfile}>{userData.initials}</ProfilePicture>
-            <ProfileName x={x} y={y}>{userData.username}</ProfileName>
-            <ProfileEmail x={x} y={y}>{userData.email}</ProfileEmail>
+            <ProfilePicture x={x} y={y} on onClick={toggleProfile}>{initials}</ProfilePicture>
+            <ProfileName x={x} y={y}>{username}</ProfileName>
+            <ProfileEmail x={x} y={y}>{email}</ProfileEmail>
             <Separator />
             <Button>Log Out</Button>
         </ProfileDDMContainer>
--- web/src/pages/Login/index.js
+++ web/src/pages/Login/index.js
@@ -54,6 +54,9 @@ const Login = () => {
                 .then((user) => {
                     if (user) {
                         sessionStorage.setItem("accessToken", user.accessToken)
+                        sessionStorage.setItem("username", user.username)
+                        sessionStorage.setItem("email", user.email)
+                        sessionStorage.setItem("initials", user.username.charAt(0).toUpperCase() + user.username.charAt(1).toUpperCase())
                         console.log('logged in. . .');
                         fetch('http://localhost:8080/dashboard', {
                             method: 'GET',
@@ -74,7 +77,6 @@ const Login = () => {
                         });
                         sessionStorage.setItem("connectTogithub", false);
                         sessionStorage.setItem("connectTodiscord", false);
-                        alert('Logged in successfully!');
                     } else {
                         console.log('error on submit ');
                         throw new Error('Something went wrong');
