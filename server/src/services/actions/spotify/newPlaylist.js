9fa04fa6ba118e91532cf75ec11d575d63643544
TitouanDsch <titouandeschanels@users.noreply.github.com>
2023-02-17 10:49:54 +0100

    [UPDATE] newPlaylist works youhou

---
+++ server/src/services/actions/spotify/newPlaylist.js
@@ -0,0 +1,54 @@
+const SpotifyWebApi = require('spotify-web-api-node');
+const mongoose = require('mongoose');
+
+const newPlaylistSchema = new mongoose.Schema({
+    name : String,
+    id : String,
+    date : Date,
+})
+
+const newPlaylist = mongoose.model('newPlaylist', newPlaylistSchema);
+
+async function newPlaylistFunc(token) {
+    const spotifyApi = new SpotifyWebApi({
+        clientId: '7e1049e74b76497a9c192fcf08c9a279',
+        clientSecret: '331c1bb09e3042f9b06a9302dc01a74c',
+        redirectUri: 'http://localhost:8080/spotifycallback'
+    });
+
+    spotifyApi.setAccessToken(token);
+
+    const username = (await spotifyApi.getMe()).body.id
+
+    const playlists = (await spotifyApi.getUserPlaylists(username)).body.items
+
+    const lastPlaylist = await newPlaylist.findOne({}).sort({date: -1})
+
+
+    if (lastPlaylist === null) {
+        const newPlaylistToStore = new newPlaylist({
+            name: playlists[0].name,
+            id: playlists[0].id,
+            date: Date.now()
+        })
+        await newPlaylistToStore.save();
+        return false
+    } else {
+        if (playlists[0].id !== lastPlaylist.id) {
+            newPlaylist.deleteMany({}, function (err) {
+                if (err) console.log(err);
+            });
+            const newPlaylistToStore = new newPlaylist({
+                name: playlists[0].name,
+                id: playlists[0].id,
+                date: Date.now()
+            })
+            await newPlaylistToStore.save();
+            return true
+        } else
+            return false
+    }
+
+}
+
+module.exports = newPlaylistFunc;
--- server/src/services/actions/spotify/spotifyActions.js
+++ server/src/services/actions/spotify/spotifyActions.js
@@ -1,14 +1,20 @@
 const lastStreamFunc = require('./lastStream');
 const createPlaylistFunc = require('./createPlaylist');
-var token = 'BQAWVNM7ZOJFIB0JYZyE1xAaHkPDsuEWBrs_yNShxleKXRL1G4hVw562qumPX_9Q0_ISHsRmOwgNt4T7bR5RZ8hqve-peAwonm19pwQoiaRiqJ9yeqqE8e8k5X4FThVj1_yn_B3T6r7M3ZT8hqhyGfYRNFPQhA5Km20U6lw4UNdS1AI9JUeT83aTziu1VjZqXbL4RqfPseoBmMGsz-lXlCeNXXZPvU0iRulIOA';
+const newPlaylistFunc = require('./newPlaylist');
+
+var token = 'BQCrQexPJ7H7pURI-uPXLQBW4q8jpA9ugqLviHR4hQqDryTgGB2ssFP07eQCzaQU7DrGaDn6SsmJtWN3zDOdOoW3zGYxnpFDvtSuI08W8uLkO4-3Fy5luYQ6P67HNbs822lTrLAscKUoul5B5I0ON3YcofWoIRRNl5bu61Qr82rEH_D2zUgMAXOVhViL0QDsnrVqMGoLLRX3XK-KYWG6xnkueOyc4bmFpVMt7pbPO6_qBbF_';
 
 const spotifyTrigger = {
     checkSpotifyAction: async function checkSpotifyAction(action) {
 
-
         if (action.trigger === 'newStream') {
             return lastStreamFunc(token)
         }
+
+        if (action.trigger === 'newPlaylist') {
+            return newPlaylistFunc(token)
+        }
+
     },
 
     checkSpotifyReaction: async function checkSpotifyReaction(reaction) {
