c8ed12d67bd5cd2024d911657c10ad982186cabb
Blynqs <baptite.friboulet@epitech.eu>
2023-02-17 18:11:31 +0100

    [FIX] Fixed github auth perms

--- web/src/components/BlocSidebar/HandleConnect.js
+++ web/src/components/BlocSidebar/HandleConnect.js
@@ -1,7 +1,7 @@
 const services = {
     github: {
         handleConnection: async function () {
-            window.location.href = `https://github.com/login/oauth/authorize?client_id=ffd70e614dd0cd62f19e`;
+            window.location.href = `http://localhost:8080/githubauth`;
         },
     },
     discord: {
