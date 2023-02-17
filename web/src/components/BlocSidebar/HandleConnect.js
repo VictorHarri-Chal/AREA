5aa5c7ad39eb14c7581e78167645c8c9f7db4199
Blynqs <baptite.friboulet@epitech.eu>
2023-02-17 17:21:02 +0100

    [UPD] updated github token

--- web/src/components/BlocSidebar/HandleConnect.js
+++ web/src/components/BlocSidebar/HandleConnect.js
@@ -1,7 +1,7 @@
 const services = {
     github: {
         handleConnection: async function () {
-            window.location.href = `https://github.com/login/oauth/authorize?client_id=4046d9ca5e2152e64fec`;
+            window.location.href = `https://github.com/login/oauth/authorize?client_id=ffd70e614dd0cd62f19e`;
         },
     },
     discord: {
