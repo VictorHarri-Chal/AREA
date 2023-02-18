ac6a6c35a103860eb1e8ad7c0c5ce87c22e53546
Blynqs <baptite.friboulet@epitech.eu>
2023-02-18 17:41:50 +0100

    [UPD] Modified push detection

--- server/src/services/actions/githubActions.js
+++ server/src/services/actions/githubActions.js
@@ -58,9 +58,41 @@ const githubTrigger = {
     },
 
     async checkNewIssues(ownerName, repoName, token) {
-        return await this.checkNewEvents("IssuesEvent", ownerName, repoName, token, (event) => {
-            console.log(`Issue: ${event.payload.issue.title}`);
-        }, true);
+        const octokit = new Octokit({ auth: token });
+        const requestOptions = {
+            owner: ownerName,
+            repo: repoName,
+            per_page: 1,
+            page: 1,
+            headers: { "If-None-Match": lastETags["IssuesEvent"] },
+        };
+
+        try {
+            const response = await octokit.issues.listForRepo(requestOptions);
+            lastETags["IssuesEvent"] = response.headers.etag;
+            const issue = response.data[0];
+
+            if (isFirstCheck) {
+                isFirstCheck = false;
+                console.log(`No new IssuesEvent events since last request.`);
+                return false;
+            }
+
+            if (issue) {
+                console.log(`Last issue detected: ${issue.title}`);
+                return true;
+            } else {
+                console.log(`No new IssuesEvent events since last request.`);
+                return false;
+            }
+        } catch (error) {
+            if (error.status === 304) {
+                console.log(`No new IssuesEvent events since last request.`);
+                return false;
+            } else {
+                throw error;
+            }
+        }
     },
 
     async checkNewPullRequests(ownerName, repoName, token) {
@@ -130,12 +162,6 @@ const githubTrigger = {
         const octokit = new Octokit({ auth: reaction.token });
 
         try {
-            // const issue = await octokit.issues.create({
-            //     ownerName,
-            //     repoName,
-            //     title: "New issue created by reaction",
-            //     body: "This issue was created in response to a reaction on an existing issue.",
-            // });
 
             const issue = await octokit.issues.create({
                 owner: ownerName,
