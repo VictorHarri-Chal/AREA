44f95e4ba70c5b6434ce29922f423cd85c36b164
Blynqs <baptite.friboulet@epitech.eu>
2023-02-18 18:15:08 +0100

    [WIP] fixed checkNewCommits and checkNewIssues ?

--- server/src/services/actions/githubActions.js
+++ server/src/services/actions/githubActions.js
@@ -19,85 +19,21 @@ const githubTrigger = {
         }
     },
 
-    async checkNewCommits(ownerName, repoName, token) {
-        const octokit = new Octokit({ auth: token });
-        const requestOptions = {
-            owner: ownerName,
-            repo: repoName,
-            per_page: 1,
-            page: 1,
-            headers: { "If-None-Match": lastETags["PushEvent"] },
-        };
-
-        try {
-            const response = await octokit.activity.listRepoEvents(requestOptions);
-            lastETags["PushEvent"] = response.headers.etag;
-            const event = response.data[0];
-
-            if (isFirstCheck) {
-                isFirstCheck = false;
-                console.log(`No new PushEvent events since last request.`);
-                return false;
-            }
-
-            if (event) {
-                console.log(`Last push event detected: ${event.payload.commits[0].message}`);
-                return true;
-            } else {
-                console.log(`No new PushEvent events since last request.`);
-                return false;
-            }
-        } catch (error) {
-            if (error.status === 304) {
-                console.log(`No new PushEvent events since last request.`);
-                return false;
-            } else {
-                throw error;
-            }
-        }
-    },
-
-    async checkNewIssues(ownerName, repoName, token) {
-        const octokit = new Octokit({ auth: token });
-        const requestOptions = {
-            owner: ownerName,
-            repo: repoName,
-            per_page: 1,
-            page: 1,
-            headers: { "If-None-Match": lastETags["IssuesEvent"] },
-        };
-
-        try {
-            const response = await octokit.issues.listForRepo(requestOptions);
-            lastETags["IssuesEvent"] = response.headers.etag;
-            const issue = response.data[0];
-
-            if (isFirstCheck) {
-                isFirstCheck = false;
-                console.log(`No new IssuesEvent events since last request.`);
-                return false;
-            }
-
-            if (issue) {
-                console.log(`Last issue detected: ${issue.title}`);
-                return true;
-            } else {
-                console.log(`No new IssuesEvent events since last request.`);
-                return false;
-            }
-        } catch (error) {
-            if (error.status === 304) {
-                console.log(`No new IssuesEvent events since last request.`);
-                return false;
-            } else {
-                throw error;
-            }
-        }
-    },
-
     async checkNewPullRequests(ownerName, repoName, token) {
         return await this.checkNewEvents("PullRequestEvent", ownerName, repoName, token, (event) => {
             console.log(`Pull Request: ${event.payload.pull_request.title}`);
+        });
+    },
+
+    async checkNewCommits(ownerName, repoName, token) {
+        return await this.checkNewEvents("PushEvent", ownerName, repoName, token, (event) => {
+            console.log(`Last push event detected: ${event.payload.commits[0].message}`);
+        });
+    },
+
+    async checkNewIssues(ownerName, repoName, token) {
+        return await this.checkNewEvents("IssuesEvent", ownerName, repoName, token, (event) => {
+            console.log(`Last issue detected: ${event.title}`);
         });
     },
 
