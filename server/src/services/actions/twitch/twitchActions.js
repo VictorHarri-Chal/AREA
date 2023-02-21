463668e4b3882f761df5f96c071c0846743e47e0
TitouanDsch <titouandeschanels@users.noreply.github.com>
2023-02-21 17:53:24 +0100

    [WIP]

---
+++ server/src/services/actions/twitch/twitchActions.js
@@ -0,0 +1,14 @@
+const twitchTrigger = {
+    checkTwitchAction : async function checkTwitchAction(action) {
+        // if (action.trigger === 'newStream') {
+        //     return lastStreamFunc(token)
+        // }
+    },
+
+    checkTwitchReaction : async function checkTwitchReaction(reaction) {
+        // if (reaction.trigger === 'createPlaylist') {
+        //     return createPlaylistFunc(token)
+        // }
+    }
+
+}
