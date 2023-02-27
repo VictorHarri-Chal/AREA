5e705d4fe33e2dfb11dcd174e609550df9b8ba99
Blynqs <baptite.friboulet@epitech.eu>
2023-02-27 11:16:05 +0100

    [WIP] Commit just for pulling

--- server/src/services/actions/youtube/youtubeActions.js
+++ server/src/services/actions/youtube/youtubeActions.js
@@ -7,6 +7,34 @@ const youtube = google.youtube({
 
 let lastLikedVideoId = null;
 
+async function checkNewLike(accessToken) {
+    try {
+        const response = await youtube.videos.list({
+            part: 'snippet',
+            myRating: 'like',
+            maxResults: 1,
+            access_token: accessToken
+        });
+        if (response.status === 200 && response.data.items.length > 0) {
+            const videoId = response.data.items[0].id;
+            const videoTitle = response.data.items[0].snippet.title;
+
+            if (videoId !== lastLikedVideoId) {
+                console.log(`User liked the video: ${videoTitle}`);
+                lastLikedVideoId = videoId;
+                return true;
+            }
+        }
+        return false;
+    } catch (error) {
+        console.log("Error: ", error);
+        if (error.response && error.response.status === 403) {
+            console.log("API key does not have permission to access user's activity feed.");
+        }
+        return false;
+    }
+}
+
 const youtubeTrigger = {
     checkYoutubeAction: async function checkYoutubeAction(action) {
         const accessToken = 'ya29.a0AVvZVsrcOs75fc0ENHx7eecpWx2_mkK9QJyeu47tvkw_V5r21Yx7yhw6bBhg7T0pFuIcnkHN2HBE9_eS2e0VGCTfc5IWArtw6qDg-UW9nXEpnPWwUvZAd77V19GP8OdQFd3904fJVrfEXLkI_wgsOi5iXu_oaCgYKAe8SARASFQGbdwaILCl0ZTHiEr6I-a9PJD4myg0163';
@@ -14,31 +42,7 @@ const youtubeTrigger = {
 
         switch (trigger) {
             case "newLike":
-            try {
-                const response = await youtube.videos.list({
-                    part: 'snippet',
-                    myRating: 'like',
-                    maxResults: 1,
-                    access_token: accessToken
-                });
-                if (response.status === 200 && response.data.items.length > 0) {
-                    const videoId = response.data.items[0].id;
-                    const videoTitle = response.data.items[0].snippet.title;
-
-                    if (videoId !== lastLikedVideoId) {
-                        console.log(`User liked the video: ${videoTitle}`);
-                        lastLikedVideoId = videoId;
-                        return true;
-                    }
-                }
-                return false;
-            } catch (error) {
-                console.log("Error: ", error);
-                if (error.response && error.response.status === 403) {
-                    console.log("API key does not have permission to access user's activity feed.");
-                }
-                return false;
-            }
+                checkNewLike(accessToken);
             default:
                 console.log(`Unsupported trigger type: ${action.trigger}`);
                 return false;
