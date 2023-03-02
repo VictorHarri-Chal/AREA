69c5db2007d326a92984fc7e26d0add8e233823f
Victor Harri-Chal <victor.harri-chal@epitech.eu>
2023-03-02 16:49:25 +0100

    [ADD] delete useless button

--- web/src/components/HeroSection/index.js
+++ web/src/components/HeroSection/index.js
@@ -1,15 +1,8 @@
 import React, {useState} from 'react'
-import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight } from './HeroElements'
+import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP } from './HeroElements'
 import Video from '../../videos/video.mp4'
-import { Button } from '../ButtonElements'
 
 const HeroSection = () => {
-
-    const [hover, setHover] = useState(false)
-
-    const onHover = () => {
-        setHover(!hover)
-    }
 
     return (
 
@@ -21,13 +14,8 @@ const HeroSection = () => {
             <HeroContent>
                 <HeroH1>The flow for your apps</HeroH1>
                 <HeroP>
-                    Welcome to the new gen web application where you can create your own action-reaction flow from multiple famous website
+                    Welcome to the new gen web application where you can create your own action-reaction flow from many famous website
                 </HeroP>
-                <HeroBtnWrapper>
-                    <Button to='signup' onMouseEnter={onHover} onMouseLeave={onHover} primary='true' dark='true'>
-                        Get started {hover ? <ArrowForward /> : <ArrowRight />}
-                    </Button>
-                </HeroBtnWrapper>
             </HeroContent>
 
         </HeroContainer>
--- web/src/pages/Home/index.js
+++ web/src/pages/Home/index.js
@@ -3,7 +3,7 @@ import PupSidebarR from '../../components/PupSidebarR'
 import Navbar from '../../components/Navbar'
 import HeroSection from '../../components/HeroSection'
 import InfoSection from '../../components/InfoSection'
-import { homeObjOne, homeObjTwo, homeObjThree } from '../../components/InfoSection/Data'
+import { homeObjOne, homeObjTwo } from '../../components/InfoSection/Data'
 import Services from '../../components/Services'
 import Footer from '../../components/Footer'
 
