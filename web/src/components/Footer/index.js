d921a3a6ca4502ce4a06eb78631f68d45b9c473f
TitouanDsch <titouandeschanels@users.noreply.github.com>
2023-03-02 14:18:04 +0100

    [HOTFIX] on my name

--- web/src/components/Footer/index.js
+++ web/src/components/Footer/index.js
@@ -12,7 +12,7 @@ const Footer = () => {
                             <FooterLinkItems>
                                 <FooterLinkTitle>About Us</FooterLinkTitle>
                                 <FooterLink to="/signin">Victor Harri-Chal</FooterLink>
-                                <FooterLink to="/signin">Titouan Deschannels</FooterLink>
+                                <FooterLink to="/signin">Titouan Deschanels</FooterLink>
                                 <FooterLink to="/signin">Baptiste Friboulet</FooterLink>
                                 <FooterLink to="/signin">Keon Savic</FooterLink>
                                 <FooterLink to="/signin">Ismael Brossaud</FooterLink>
@@ -21,7 +21,7 @@ const Footer = () => {
                             <FooterLinkItems>
                                 <FooterLinkTitle>Contact Us</FooterLinkTitle>
                                 <FooterLink to="/signin">victor.harri-chal@epitech.eu</FooterLink>
-                                <FooterLink to="/signin">titouan.deschannels@epitech.eu</FooterLink>
+                                <FooterLink to="/signin">titouan.deschanels@epitech.eu</FooterLink>
                                 <FooterLink to="/signin">baptiste.friboulet@epitech.eu</FooterLink>
                                 <FooterLink to="/signin">keon.savic@epitech.eu</FooterLink>
                                 <FooterLink to="/signin">ismael.brossaud@epitech.eu</FooterLink>
