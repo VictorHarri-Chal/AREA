222ff5403c4953c31bb27651b19f2373fdb33b26
Blynqs <baptite.friboulet@epitech.eu>
2023-03-05 22:17:28 +0100

    [UPD] about.json

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -24,36 +24,104 @@ exports.about = (req, res) => {
                     name: "github",
                     actions: [
                         {
-                            name: "new_issue_on_a_repository",
+                            name: "newIssue",
                             description: "A new issue is opened on a specific repository"
                         },
                         {
-                            name: "new_push_on_a_repository",
-                            description: "A new push on a specific repository"
+                            name: "newCommit",
+                            description: "A new commit on a specific repository"
                         },
                     ],
                     reactions: [
                         {
-                            name: "new_issue_on_a_repository",
+                            name: "createIssue",
                             description: "Creates an issue on a specific repository"
                         }
+                    ]
+                },
+                {
+                    name: "discord",
+                    actions: [
+                        {
+                            name: "receivePrivateMessage",
+                            description: "Receives a private message"
+                        },
                     ],
+                    reactions: [
+                        {
+                            name: "sendPrivateMessage",
+                            description: "Sends a private message"
+                        },
+                        {
+                            name: "createNewGuild",
+                            description: "Creates a new guild"
+                        }
+                    ]
+                },
+                {
+                    name: "twitter",
+                    actions: [],
+                    reactions: [
+                        {
+                            name: "followRandomUser",
+                            description: "Follows a random user"
+                        },
+                    ]
+                },
+                {
                     name: "spotify",
                     actions: [
                         {
-                            name: "",
-                            description: ""
+                            name: "newStream",
+                            description: "New stream"
                         },
                         {
-                            name: "",
-                            description: ""
+                            name: "newPlaylist",
+                            description: "New playlist"
                         },
                     ],
                     reactions: [
                         {
-                            name: "",
-                            description: ""
-                        }
+                            name: "createPlaylist",
+                            description: "Creates a playlist"
+                        },
+                        {
+                            name: "pausePlayback",
+                            description: "Pauses playback"
+                        },
+                        {
+                            name: "randomTrack",
+                            description: "Adds random track to queue"
+                        },
+                    ]
+                },
+                {
+                    name: "youtube",
+                    actions: [
+                        {
+                            name: "newLike",
+                            description: "New like on a video"
+                        },
+                        {
+                            name: "newVideo",
+                            description: "New video uploaded"
+                        },
+                    ],
+                    reactions: []
+                },
+                {
+                    name: "twitch",
+                    actions: [
+                        {
+                            name: "onStream",
+                            description: "Specific streamer is live"
+                        },
+                    ],
+                    reactions: [
+                        {
+                            name: "banRandom",
+                            description: "Bans a random user"
+                        },
                     ]
                 }
             ]
