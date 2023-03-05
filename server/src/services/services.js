5286049288894bb8faa9bb13a2e1ed25af9ebd35
Blynqs <baptite.friboulet@epitech.eu>
2023-03-05 15:58:57 +0100

    [ADD] twitter reaction followRandomUser

--- server/src/services/services.js
+++ server/src/services/services.js
@@ -1,13 +1,15 @@
-const githubTrigger = require('./actions/githubActions');
+const githubTrigger = require('./actions/github/githubActions');
 const discordTrigger = require('./actions/discord/discordActions');
-const spotifyTrigger = require('./actions/spotify/spotifyActions')
-const youtubeTrigger = require('./actions/youtube/youtubeActions')
-const twitchTrigger = require('./actions/twitch/twitchActions')
+const spotifyTrigger = require('./actions/spotify/spotifyActions');
+const youtubeTrigger = require('./actions/youtube/youtubeActions');
+const twitchTrigger = require('./actions/twitch/twitchActions');
+const twitterTrigger = require('./actions/twitter/twitterActions');
 
 const services = {
     github: {
         checkTrigger: async function(action) {
-            // return await githubTrigger.checkGithubAction(action);
+            console.log("GITHUB ACTION");
+            return await githubTrigger.checkGithubAction(action);
         },
         startReaction: async function(reaction) {
             // return await githubTrigger.checkGithubReaction(reaction);
@@ -40,10 +42,20 @@ const services = {
     },
     youtube: {
         checkTrigger: async function(action) {
+            console.log("YOUTUBE ACTION");
             // return await youtubeTrigger.checkYoutubeAction(action);
         },
         startReaction: async function(reaction) {
             // return await youtubeTrigger.youtubeReaction(reaction);
+        }
+    },
+    twitter: {
+        checkTrigger: async function(action) {
+            // return await twitterTrigger.checkTwitterAction(action);
+        },
+        startReaction: async function(reaction) {
+            console.log("TWITTER REACTION");
+            return await twitterTrigger.checkTwitterReaction(reaction);
         }
     }
 };
